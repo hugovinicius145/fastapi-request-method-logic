e9c93f08b60edc99de70673be6a8853b7d8df72f
Hugo <hugovinicius145@gmail.com>
2023-08-09 11:10:12 -0300

    Post Request

--- books.py
+++ books.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import Body, FastAPI
 
 app = FastAPI()
 
@@ -39,3 +39,8 @@ async def read_author_category_by_query(book_author: str, category: str):
     if book.get('author').casefold() == book_author.casefold() and book.get('category').casefold() == category.casefold():
       books_to_return.append(book)
   return books_to_return  
+
+
+@app.post("/books/create_book")
+async def create_book(new_book=Body()):
+  BOOKS.append(new_book)
